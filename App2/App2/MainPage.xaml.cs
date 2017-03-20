8a9c20f97e67188245a3565641391f47e3950c76
Milan Laslop <milan33@gmail.com>
2017-03-20 09:38:31 +0100

    initial, Xamarin Forms 2.3.3.193

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            label1.Text = "abc";
+        }
+    }
+}
